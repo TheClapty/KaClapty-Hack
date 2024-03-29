6a54eb44c8898bbc1682a0380ac2198b4f0e4fa5
TheClapty <128987251+TheClapty@users.noreply.github.com>
2024-03-29 22:29:55 +0100

    KaClapty!.meta.js

---
+++ KaClapty!.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         KaClapty!
+// @version      1.0.23
+// @namespace    https://github.com/TheClapty
+// @description  A hack for kahoot.it by TheClapty!
+// @updateURL    https://github.com/TheClapty/KaClapty-Hack/raw/main/
+// @downloadURL  https://github.com/TheClapty/KaClapty-Hack/raw/main/KaClapty!.user.js
+// @author       TheClapty; https://github.com/TheClapty
+// @match        https://kahoot.it/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
+// @grant        none
+// ==/UserScript==
